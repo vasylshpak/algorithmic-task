21d2fa488870ce396b35add83346d5b8c6b0c9b0
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-10-16 09:38:51 +0200

    update

--- algorithmictask.js
+++ algorithmictask.js
@@ -4,10 +4,13 @@ const num = Math.floor(Math.random() * 100);
 
 const checkIfTree = (arg) => {
   let arg2 = arg * arg;
-  const arrayOfDigits = Array.from(String(arg2), Number);
   console.log(arg2);
+  const arrayOfDigits = Array.from(String(arg2), Number);
   for (let i = 0; i < arrayOfDigits.length; i++) {
     if (arrayOfDigits[i] == 3) return true;
+    else {
+      return false;
+    }
   }
 };
 
@@ -16,18 +19,21 @@ console.log("Чи входить число 3 в запис числа n2?", che
 // б) Поменять порядок цифр числа n на обратный.
 const reverseNum = (arg) => {
   arg = arg + "";
-  return arg.split("").reverse().join("");
+  console.log(arg);
+  return +arg.split("").reverse().join("");
 };
 console.log("Зміна порядку числа", reverseNum(num));
 
 const maxDivider = () => {};
 //322 Найти натуральное число от 1 до 10 000 с максимальной суммой делителей.
 const bigNum = Math.floor(Math.random() * 10000);
+console.log(bigNum);
 function listDivisors(n) {
-  let small = [];
-  let large = [];
+  const small = [];
+  const large = [];
   const end = Math.floor(Math.sqrt(n));
   for (let i = 1; i <= end; i++) {
+    //
     if (n % i == 0) {
       small.push(i);
       if (i * i != n) large.push(n / i);
